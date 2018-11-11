ab9e17d2992e647ff3e333a68bf65eef6d49287b
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-11 08:27:18 -0600

    unfinished lab

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
+//STUCK BC I NEED TO INSERT 4 MORE FRIENDS, BUT BROWSER KEEPS CRASHING WHEN ADDING TO ARRAY
+//STUCK BC I NEED TO FIGURE OUT HOW TO FIX '1 LINE OF CODE' INSTEAD OF PLURAL - MAYBE ANOTHER LOOP???
 const list = ['John']
 for (var i = 0; i < list.length; i++) {
       console.log(list[i])
 
       for (i = 99; i > 0; i--) {
-        console.log([i] + ' lines of code on the wall ' + [i] + ' lines of code. One fell off and made a fall');
+        console.log([i] + ' lines of code on the wall, ' + [i] + ' lines of code. One fell off and made a fall ')
+        console.log( ' now ' + [i-1] + ' lines of code remain on the wall! ');
 
     }
     
