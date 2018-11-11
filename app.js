e7a96d3dfd7828f941f8a27594a37c2458b0e28c
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-11 07:52:58 -0600

    working lab

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
+const list = ['John', 'Sally', 'Jaime', 'Kim', 'Liz']
+for (var i = 0; i < list.length; i++) {
+      console.log(list[i])
 
-for (let i=0; i < 99; i++) {
-    console.log(i);
+      for (i = 99; i > 0; i--) {
+        console.log([i] + ' lines of code on the wall ' + [i] + ' lines of code. One fell off and made a fall, now there is ' + [i--] +  ' lines of code left on the wall!' );
+    }
+    
 }
