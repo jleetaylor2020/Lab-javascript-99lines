d2c5ea2a8db8c5694d7609fb6320e84d571552c6
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-11 08:14:49 -0600

    working on fixing 99 lines of code lab

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-const list = ['John', 'Sally', 'Jaime', 'Kim', 'Liz']
+const list = ['John']
 for (var i = 0; i < list.length; i++) {
       console.log(list[i])
 
       for (i = 99; i > 0; i--) {
-        console.log([i] + ' lines of code on the wall ' + [i] + ' lines of code. One fell off and made a fall, now there is ' + [i--] +  ' lines of code left on the wall!' );
+        console.log([i] + ' lines of code on the wall ' + [i] + ' lines of code. One fell off and made a fall');
+
     }
     
 }
